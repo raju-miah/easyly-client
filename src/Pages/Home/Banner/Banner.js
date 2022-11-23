92947d733dafb1025dde1a0f497ba4c5933ba772
raju-miah <rajumiahpollob7@gmail.com>
2022-11-24 02:16:01 +0600

    added navbar and footer

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <div className="hero bg-amber-200 mt-10">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <img alt='' src="https://i.ibb.co/LJmYTwm/home-1.jpg" className=" rounded-lg lg:w-1/2 shadow-2xl" />
+                    <div>
+                        <h1 className="text-5xl font-bold">EASYLY</h1>
+                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <button className="btn btn-accent">Sing Up Free</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
